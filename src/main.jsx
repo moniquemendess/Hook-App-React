cfa548f3443c42f6cb371c75f18920d0eac928a7
Monique Mendes <moniiquemendes96@gmail.com>
2024-06-13 11:42:12 +0200

    useState con el counter

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { CounterApp } from "./01-useState/CounterApp";
+// import { HooksApp } from "./HooksApp";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <CounterApp />
+  </React.StrictMode>
+);
