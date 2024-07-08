e5fd1912f5d19de4ebf79f311b76e2ed36ca022d
Monique Mendes <moniiquemendes96@gmail.com>
2024-07-08 10:47:51 +0200

    realizacion de peticiones

--- src/main.jsx
+++ src/main.jsx
@@ -5,10 +5,11 @@ import "./index.css";
 // import { HooksApp } from "./HooksApp";
 //import { CounterWithCustomHook } from "./01-useState/CounterWithCustomHook";
 //import { SimpleForm } from "./02-useEffect/SimpleForm";
-import { FormWithCustomHook } from "./02-useEffect/FormWithCustomHook";
+//import { FormWithCustomHook } from "./02-useEffect/FormWithCustomHook";
+import { MultipleCustomHooks } from "./03-examples/MultipleCustomHooks";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <FormWithCustomHook />
+    <MultipleCustomHooks />
   </React.StrictMode>
 );
